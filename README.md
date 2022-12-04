4e6583acdea6592fd12dbf4b754912fca3494d86
cybrd <brysuralta@gmail.com>
2022-12-04 23:28:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Scrum / Kanban Board
+
+## to do
+
+- [x] task view
+- [ ] save task
+- [ ] create front end page w/o function
---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+export default () => {
+  return (
+    <div className="p-5">
+      <table className="w-full table-auto border-spacing-2">
+        <tbody>
+          <tr>
+            <td className="w-40 text-right p-3">Summary</td>
+            <td>
+              <input
+                className="w-full bg-gray-200 text-gray-700 border p-2 focus:outline-none focus:bg-white"
+                placeholder="Summary"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="w-40 text-right p-3">Status</td>
+            <td>
+              <select className="bg-gray-200 border border-gray-200 text-gray-700 p-2 focus:outline-none focus:bg-white focus:border-gray-500">
+                <option>Backlog</option>
+                <option>To do</option>
+                <option>In Progress</option>
+                <option>In QA</option>
+                <option>Done</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td className="w-40 text-right p-3">Description</td>
+            <td>
+              <textarea className="w-full h-40 bg-gray-200 text-gray-700 border resize-none p-2 focus:outline-none focus:bg-white" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="grid justify-items-center pt-5">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
